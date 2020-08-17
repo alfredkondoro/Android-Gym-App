1b0c07d1758b5adf9b5644019fc0a990bc49b780
Alfred Kondoro <alfredkondoro97@gmail.com>
2020-08-18 02:22:35 +0300

    version 0.21

--- app/src/main/java/com/example/infinitygym/MainActivity.java
+++ app/src/main/java/com/example/infinitygym/MainActivity.java
@@ -4,23 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
-
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        handler=new Handler ();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, register.class);
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,login.class);
                 startActivity(intent);
+                finish();
             }
-        });
+        },3000);
+
     }
 }
